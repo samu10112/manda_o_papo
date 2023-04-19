d24140af69d315490254d57d7a1ea9814d3886af
samu10112 <119002488+samu10112@users.noreply.github.com>
2023-04-19 18:55:21 -0300

    Add files via upload

--- papo.js
+++ papo.js
@@ -27,12 +27,36 @@ function getData(){
     document.getElementById("output").innerHTML="";
     snapshot.forEach(function(childSnapshot){
       var childkey=childSnapshot.key;
-      var roomName=childkey;
+      var childdata=childSnapshot.val;
+      if(childkey!="purpose"){
+        var fire_id=childkey;
+        var message_data=child;
+        var name_base=message_data["name"];
+        var message_base=message_data["message"];
+        var like_base=message_data["like"];
+
+        var tag_name="<h4>"+name_base+"<img class='user_tick'src='manda_o _papo.jpeg'></h4>";
+        var tag_mensagem="<h4 class='message_h4'>"+message_base+"</h4>";
+        var tag_botao="<button class='btn btn-info' id='"+fire_id+"' value='"+like_base+"' onclick='atualizalike(this.id)'>";
+        var tag_span="<span class='glyphicon glyphicon-thumbs-up'>like:"+like_base+"</span></button><hr>";
+        var caixa=tag_name+tag_mensagem+tag_botao+tag_span;
         document.getElementById("output").innerHTML+=caixa;
+      }
     }
   );}
 );}
 getData();
+
+function atualizalike(id_da_mensagem){
+  console.log("o botão de like foi apertado- "+id_da_mensagem);
+  var id_do_botao=id_da_mensagem;
+  var likes=document.getElementById(id_do_botao).value;
+  var atualiza_like=Number(likes)+1;
+  firebase.database().ref(nome_da_sala).child(id_da_mensagem).update({
+    like:atualiza_like
+  });
+}
+
 function logout(){
   localStorage.removeItem("nome_sala");
   localStorage.removeItem("nome_usuario");
